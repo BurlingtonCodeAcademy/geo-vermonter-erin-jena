76c4941d8b74fe4110b1ba185693c3f8ce1b0a80
Erin Soggs <erinasoggs@gmail.com>
2020-07-09 16:13:29 -0400

    finished getting random lat and long numbers

--- src/App.js
+++ src/App.js
@@ -6,24 +6,41 @@ export class App extends Component {
         super()
 
         this.state = {
-            
+          gameStarted: false,
+          long: -72.7317,
+          lat: 44.0886
         }
     }
 
-    startButton = (evt) => {
-        evt.preventDefault()
-        
-        
+    randomLatLong = (minLat, maxLat, minLong, maxLong) => {
+      let latCoords = Math.random() * (45.005419 - 42.730315 + 1) + 42.730315);
+      let longCoords = Math.random() * (-71.510225 - (-73.352182 + 1)) + (-73.352182);
+      this.setState({
+        long: longCoords,
+        lat: latCoords
+      })
     }
+    
+  startButton = (evt) => {
+        evt.preventDefault()
+        this.setState({
+          gameStarted: true
+    })
+  }
 
-    render() {
+  componentDidMount() {
+    this.randomLatLong()
+  }
+    
+  render() {
+      console.log(this.state)
         return (
             <div>
                 <VTMap />
                 <div>
-                <button onClick={this.startButton}>Start Game</button>
-                <button onclick={this.giveUpButton}>Give Up</button>
-                <button onClick={this.guessButton}>Guess</button>
+                <button disabled={this.state.gameStarted} onClick={this.startButton}>Start Game</button>
+                <button disabled={!this.state.gameStarted} onclick={this.giveUpButton}>Give Up</button>
+                <button disabled={!this.state.gameStarted} onClick={this.guessButton}>Guess</button>
                 </div>
 
             </div>
