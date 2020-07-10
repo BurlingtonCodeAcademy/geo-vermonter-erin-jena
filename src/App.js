fe0dcc0511935155916096b98880db9da70169bf
Erin Soggs <erinasoggs@gmail.com>
2020-07-10 15:39:52 -0400

    we changed place marker, random lat long functions

--- src/App.js
+++ src/App.js
@@ -1,59 +1,89 @@
-import React, { Component } from 'react'
-import VTMap from './VTMap'
+import React, { Component } from "react"
+import VTMap from "./VTMap"
+import leafletPip from "@mapbox/leaflet-pip"
+import L from "leaflet"
+import borderData from "./border.js"
 
 export class App extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            gameStarted: false,
-            long: -72.7317,
-            lat: 44.0886
-        }
+    this.state = {
+      gameStarted: false,
+      long: -72.7317,
+      lat: 44.0886,
     }
+  }
 
-    randomLatLong = (minLat, maxLat, minLong, maxLong) => {
-        let latCoords = Math.random() * (45.005419 - 42.730315 + 1) + 42.730315;
-        let longCoords = Math.random() * (-71.510225 - (-73.352182 + 1)) + (-73.352182);
-        this.setState({
-            long: longCoords,
-            lat: latCoords
-        })
+  randomLatLong = (minLat, maxLat, minLong, maxLong) => {
+    console.log("inside latlong")
+    let latCoords = (Math.random() * (45.005419 - 42.730315) + 42.730315)
+    let longCoords = (Math.random() * (71.510225 - (73.352182)) + 73.352182) * -1
+      console.log(longCoords)
+      console.log(latCoords)
+
+    return ({
+      long: longCoords,
+      lat: latCoords,
+    })
+  }
+  
+  placeMarker = (evt) => {
+     console.log("inside place marker")
+    let gjLayer = L.geoJSON(borderData)
+    let point = this.randomLatLong()
+
+    let results = leafletPip.pointInLayer(
+      [point.long, point.lat],
+      gjLayer
+    )
+    
+
+    // let point = [this.state.long, this.state.lat]
+
+    while (results.length < 1) {
+      console.log("inside loop")
+      point = this.randomLatLong()
+      console.log(this.state.lat)
+      console.log(this.state.long)
+      results = leafletPip.pointInLayer([point.long, point.lat],gjLayer)
+      console.log(results)
     }
+    this.setState(point)
+  }
 
-    placeMarker = (latLongArray, pinLink) => {
+  startButton = (evt) => {
+    evt.preventDefault()
+    this.setState({
+      gameStarted: true,
+    })
+  }
 
-        let spot = .marker(latLongArray).addTo(myMap)
-        spot.bindPopup(pinLink)
+  componentDidMount() {
+    this.placeMarker()
+  }
 
-    }
-
-    startButton = (evt) => {
-        evt.preventDefault()
-        this.setState({
-            gameStarted: true
-        })
-    }
-
-    componentDidMount() {
-        this.randomLatLong()
-    }
-
-    render() {
-        console.log(this.state)
-        return (
-            <div>
-                <VTMap />
-                <div>
-                    <button disabled={this.state.gameStarted} onClick={this.startButton}>Start Game</button>
-                    <button disabled={!this.state.gameStarted} onclick={this.giveUpButton}>Give Up</button>
-                    <button disabled={!this.state.gameStarted} onClick={this.guessButton}>Guess</button>
-                </div>
-
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <VTMap />
+        <div>
+          <button disabled={this.state.gameStarted} onClick={this.startButton}>
+            Start Game
+          </button>
+          <button
+            disabled={!this.state.gameStarted}
+            onClick={this.giveUpButton}
+          >
+            Give Up
+          </button>
+          <button disabled={!this.state.gameStarted} onClick={this.guessButton}>
+            Guess
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/VTMap.js
+++ src/VTMap.js
@@ -1,39 +1,36 @@
-import React from 'react'
-import { Map, TileLayer, Polygon } from 'react-leaflet'
-import borderData from './border.js'
-
+import React from "react"
+import { Map, TileLayer, Polygon } from "react-leaflet"
+import borderData from "./border.js"
 
 class VTMap extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            
-        }
-    }
+    this.state = {}
+  }
 
-    render() {
-       
-        let vtBorder = borderData.geometry.coordinates[0].map(coordSet => {
-            return [coordSet[1], coordSet[0]]
-        })
-        
+  render() {
+    let vtBorder = borderData.geometry.coordinates[0].map((coordSet) => {
+      return [coordSet[1], coordSet[0]]
+    })
 
-        return (
-       
-            <Map center={[43.85, -72.7317]} zoom={8} style={{height: '650px', width: '550px'}}>
-                <TileLayer
-                    url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-                    attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community' />
-
-                <Polygon positions={vtBorder} />
-            </Map>
-            
-        );
-
-
-    }
+    return (
+      <Map
+        center={[43.85, -72.7317]}
+        zoom={8}
+        style={{
+          height: "650px",
+          width: "550px",
+        }}
+      >
+        <TileLayer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+        />
+        <Polygon positions={vtBorder} />{" "}
+      </Map>
+    )
+  }
 }
 
 export default VTMap
-
